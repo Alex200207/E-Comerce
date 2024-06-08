b9afc3f90629ba5681ab718e04234047c25edb70
soy eddy <soyeddy.email.com>
2024-06-08 13:41:38 -0600

    first commit

---
+++ Backend/database.ts
@@ -0,0 +1,18 @@
+// backend/database.ts
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
+  {
+    host: process.env.DB_HOST!,
+    dialect: 'mysql',
+    port: parseInt(process.env.DB_PORT!, 10),
+  }
+);
+
+export default sequelize;
