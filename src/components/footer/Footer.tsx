132437e8ddd7d352929bd83dd312e0c66a41b39d
soy eddy <soyeddy.email.com>
2024-07-11 10:43:42 -0600

    ajustando el estilo del footer y pagina

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,27 +1,210 @@
-import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faGooglePlus,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faHome,
+  faEnvelope,
+  faPhone,
+  faPrint,
+} from "@fortawesome/free-solid-svg-icons";
 
-const Footer: React.FC = () => {
+const Footer = () => {
   return (
-    <footer className="footer mt-auto py-3 text-white footer-custom">
-      <div className="container">
-        <span className="text-white">
-          2024 AlexStore. Todos los derechos reservados.
-        </span>
+    <footer className="bg-dark text-white pt-5 pb-4">
+      <div className="container text-center text-md-left">
+        <div className="row text-center text-md-left">
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Tu Tienda
+            </h5>
+            <p>
+              Tu tienda de confianza para comprar los mejores productos a los
+              mejores precios.
+            </p>
+          </div>
 
-        <p>
-          Puedes encontrarme en Facebook
-          <a href="https://www.facebook.com/alexis.talavera.503?locale=es_LA">
-            {" "}
-            Click Aqui
-          </a>
-        </p>
-        <p>
-          Puedes encontrarme en Github
-          <a href="https://www.facebook.com/alexis.talavera.503?locale=es_LA">
-            {" "}
-            Click Aqui
-          </a>
-        </p>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Productos
+            </h5>
+            <p>
+              <a
+                href="#"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Electrónica
+              </a>
+            </p>
+            <p>
+              <a
+                href="#"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Ropa
+              </a>
+            </p>
+            <p>
+              <a
+                href="#"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Accesorios
+              </a>
+            </p>
+            <p>
+              <a
+                href="#"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Ofertas
+              </a>
+            </p>
+          </div>
+
+          <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Enlaces útiles
+            </h5>
+            <p>
+              <a
+                href="#"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Tu cuenta
+              </a>
+            </p>
+            <p>
+              <a
+                href="#"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Ayuda
+              </a>
+            </p>
+            <p>
+              <a
+                href="#"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Envíos
+              </a>
+            </p>
+            <p>
+              <a
+                href="#"
+                className="text-white"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Políticas de devolución
+              </a>
+            </p>
+          </div>
+
+          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Contacto
+            </h5>
+            <p>
+              <FontAwesomeIcon icon={faHome} className="mr-3" /> Nicaragua
+              ,Nueva Segovia , Jalapa
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faEnvelope} className="mr-3" />{" "}
+              AlexStore@gmail.com
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faPhone} className="mr-3" /> + 505 88445304
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faPrint} className="mr-3" /> + 505 88445304
+            </p>
+          </div>
+        </div>
+
+        <hr className="mb-4" />
+
+        <div className="row align-items-center">
+          <div className="col-md-7 col-lg-8">
+            <p>
+              © 2024 Todos los derechos reservados por:
+              <a href="#" style={{ textDecoration: "none" }}>
+                <strong className="text-warning"> AlexStore</strong>
+              </a>
+            </p>
+          </div>
+          <div className="col-md-5 col-lg-4">
+            <div className="text-center text-md-right">
+              <ul className="list-unstyled list-inline">
+                <li className="list-inline-item">
+                  <a
+                    href="#"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <FontAwesomeIcon icon={faFacebook} />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="#"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="#"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <FontAwesomeIcon icon={faGooglePlus} />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="#"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="#"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <FontAwesomeIcon icon={faInstagram} />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
